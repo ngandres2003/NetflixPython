baf8a26ec0bf7719021d05747076e96f36cbdad2
Andres Ng <ngandres28@gmail.com>
2023-03-18 15:21:04 -0400

    Ultimo cambio

--- utils.py
+++ utils.py
@@ -19,7 +19,7 @@ def top_3(data): #Funcion para encontrar top 3 con mejor puntuacion
     movies = list(map(lambda movie : movie['Title'],data)) #Lista de las peliculas
     dict_top3 = dict(zip(movies[-3:],imdb[-3:])) #Diccionario con las top 5 peliculas
     return dict_top3.keys(),dict_top3.values()
-        
+       
     
 
     
